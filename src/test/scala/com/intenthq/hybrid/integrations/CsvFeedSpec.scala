a097ffdfc810c280881bddd68759e7b1bc37b2a5
Merlin Rabens <merlin.rabens@intenthq.com>
2020-10-26 10:31:13 +0100

    #2309 Rename test description
    Signed-off-by: Merlin Rabens <merlin.rabens@intenthq.com>

--- src/test/scala/com/intenthq/hybrid/integrations/CsvFeedSpec.scala
+++ src/test/scala/com/intenthq/hybrid/integrations/CsvFeedSpec.scala
@@ -9,7 +9,7 @@ object CsvFeedSpec extends SimpleIOSuite {
     "447599999999,20150620000000,microsoft.co.uk,3"
   ).map(_ + '\n')
 
-  simpleTest("should return a stream of parsed O2 weblog feed rows") {
+  simpleTest("should return a stream of parsed csv feed rows") {
     for {
       rowsBytes <- ExampleLocalFileCsvFeed.stream(SourceContext.empty).compile.toList
       rows = rowsBytes.map(new String(_)).toSet
