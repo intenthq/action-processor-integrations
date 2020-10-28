3820e9918367826e67abbaaec3979dde8b59f868
Merlin Rabens <merlin.rabens@intenthq.com>
2020-10-28 15:59:52 +0100

    #2309 Remove default implementation for serialize
    Signed-off-by: Merlin Rabens <merlin.rabens@intenthq.com>

--- src/main/scala/com/intenthq/action_processor/integrationsV2/HiveSource.scala
+++ src/main/scala/com/intenthq/action_processor/integrationsV2/HiveSource.scala
@@ -1,14 +1,12 @@
 package com.intenthq.hybrid.integrationsV2
 
-import java.nio.charset.StandardCharsets
-
 import cats.effect._
 import com.intenthq.hybrid.integrations.{JavaLegacyTimeMeta, TimeMeta}
 import doobie.util.transactor.{Strategy, Transactor}
 
 import scala.util.Properties
 
-abstract class HiveSource[O] extends Processor with SQLSource[O] with Aggregations[O] with TimeMeta with JavaLegacyTimeMeta {
+abstract class HiveSource[O] extends SQLSource[O] with Aggregations[O] with TimeMeta with JavaLegacyTimeMeta {
 
   override protected val driver: String = "org.apache.hive.jdbc.HiveDriver"
 
@@ -16,6 +14,5 @@ abstract class HiveSource[O] extends Processor with SQLSource[O] with Aggregatio
 
   override protected lazy val transactor: Transactor[IO] = Transactor.strategy.set(createTransactor, Strategy.void)
 
-  override def serialize(o2: (String, Long)): Array[Byte] = s"${o2._1},${o2._2}\n".getBytes(StandardCharsets.UTF_8)
-
+  override def serialize(o2: (String, Long)): Array[Byte]
 }
