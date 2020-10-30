331a0af21daaa4142a483c374484a15faa5225b8
Merlin Rabens <merlin.rabens@intenthq.com>
2020-10-30 13:25:22 +0100

    Next evolutionary step

--- src/main/scala/com/intenthq/action_processor/integrations/serializations/csv/CsvSerialization.scala
+++ src/main/scala/com/intenthq/action_processor/integrations/serializations/csv/CsvSerialization.scala
@@ -4,18 +4,15 @@ import java.io._
 import java.nio.charset.StandardCharsets
 
 import cats.effect.IO
-import de.siegmar.fastcsv.writer.CsvWriter
+//import de.siegmar.fastcsv.writer.CsvWriter
+import com.opencsv.CSVWriter
 
 import scala.collection.immutable.ArraySeq
 
 object CsvSerialization {
 
   val lineDelimiter: String = "\n"
-  private val csvWriter = {
-    val writer = new CsvWriter()
-    writer.setLineDelimiter(lineDelimiter.toCharArray)
-    writer
-  }
+  private val csvWriter = new CSVWriter(new StringWriter())
 
   def serialize[O](o: O)(implicit csv: Csv[O]): IO[Array[Byte]] =
     IO.delay {
