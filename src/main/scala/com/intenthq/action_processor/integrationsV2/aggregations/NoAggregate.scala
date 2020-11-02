c8019544a0854110187438a7b71d619685c93953
Merlin Rabens <merlin.rabens@intenthq.com>
2020-11-02 20:31:58 +0100

    Add feed context to transform method
    Signed-off-by: Merlin Rabens <merlin.rabens@intenthq.com>

--- src/main/scala/com/intenthq/action_processor/integrationsV2/aggregations/NoAggregate.scala
+++ src/main/scala/com/intenthq/action_processor/integrationsV2/aggregations/NoAggregate.scala
@@ -1,9 +1,11 @@
 package com.intenthq.action_processor.integrationsV2.aggregations
 
 import cats.effect.IO
-import com.intenthq.action_processor.integrationsV2.feeds.Feed
+import com.intenthq.action_processor.integrationsV2.feeds.{Feed, FeedContext}
+
+import scala.annotation.unused
 
 trait NoAggregate[I] {
   self: Feed[I, I] =>
-  override def transform: fs2.Pipe[IO, I, (I, Long)] = Aggregate.noop
+  override def transform(@unused feedContext: FeedContext[IO]): fs2.Pipe[IO, I, (I, Long)] = Aggregate.noop
 }
