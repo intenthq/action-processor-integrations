003dabd8aa12d5db4115cba33c28738dc473ef30
Merlin Rabens <merlin.rabens@intenthq.com>
2020-11-03 16:41:36 +0100

    #2309 Add missing feed filter methods
    Signed-off-by: Merlin Rabens <merlin.rabens@intenthq.com>

--- src/main/scala/com/intenthq/action_processor/integrations/feeds/Feed.scala
+++ src/main/scala/com/intenthq/action_processor/integrations/feeds/Feed.scala
@@ -1,8 +1,15 @@
 package com.intenthq.action_processor.integrations.feeds
 
+import java.time.{Clock, LocalDate}
+
 import cats.effect.IO
 
 trait Feed[I, O] {
+
+  lazy val feedName: String = getClass.getSimpleName.stripSuffix("$")
+  def date(feedContext: FeedContext[IO], clock: Clock = Clock.systemDefaultZone()): IO[LocalDate] =
+    feedContext.filter.date.fold(IO.delay(java.time.LocalDate.now(clock)))(IO.pure)
+  def part(feedContext: FeedContext[IO]): Int = feedContext.filter.time.fold(0)(_.getHour)
 
   def inputStream(feedContext: FeedContext[IO]): fs2.Stream[IO, I]
   def transform(feedContext: FeedContext[IO]): fs2.Pipe[IO, I, (O, Long)]
