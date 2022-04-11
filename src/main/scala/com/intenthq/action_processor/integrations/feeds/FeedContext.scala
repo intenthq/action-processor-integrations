de4595278ecc7afd09de00b7c860b02abc3b4a3d
joseraya <jose.raya@intenthq.com>
2022-04-11 09:10:25 +0200

    PFM-684 Adds a partition attribute to the filter
    This will allow users of the action processor to partition their queries by any arbitrary value, not just the date and time.

--- src/main/scala/com/intenthq/action_processor/integrations/feeds/FeedContext.scala
+++ src/main/scala/com/intenthq/action_processor/integrations/feeds/FeedContext.scala
@@ -6,9 +6,9 @@ import com.intenthq.embeddings.Mapping
 
 import java.time.{LocalDate, LocalTime}
 
-case class FeedFilter(date: Option[LocalDate], time: Option[LocalTime])
+case class FeedFilter(date: Option[LocalDate], time: Option[LocalTime], partition: Map[String, String])
 object FeedFilter {
-  val empty: FeedFilter = FeedFilter(None, None)
+  val empty: FeedFilter = FeedFilter(None, None, Map.empty)
 }
 case class FeedContext[F[_]](embeddings: Option[Mapping[String, List[String], F]],
                              filter: FeedFilter,
